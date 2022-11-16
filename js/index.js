d8cc03a69905d4619421cff385e5c122c37d4c49
Jonnathan <jonzxz12@gmail.com>
2022-11-16 17:37:29 -0600

    Redireccion a el nuevo projecto ajustado - 11/16/2022 - 5:37PM

--- js/index.js
+++ js/index.js
@@ -9,3 +9,8 @@ function quitAnimation(element){
     div.classList.remove('card-animationIn');
     div.classList.add('card-animationOut');
 }
+
+const projectList = document.querySelectorAll('ul.proyects-list li')
+projectList[0].addEventListener('click', () =>{
+    window.location.href = "https://jonnathan-todo-list.netlify.app/"
+})
