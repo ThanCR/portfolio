57e50e098c8d3f5ab5daa209997b8e94b23d4994
Jonnathan <jonzxz12@gmail.com>
2024-09-19 17:43:34 -0600

    Header finished

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,9 @@ import './Header.css';
 function Header() {
   return (
     <div className="Header">
-      <h1>Header</h1>
+      <h1>Jonnathan Ríos</h1>
+      <p>Full Stack Developer</p>
+      <button>Continue</button>
     </div>
   );
 }
